b5d239346c8e36ab0ef84b418e0a8201b5195f84
Sador <Zerezghi@live.co.uk>
2024-09-19 06:28:53 +0100

    Changed Header to Background Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from "./components/Header.jsx"
+import Background from "./components/Background.jsx"
 import "./styles/Base.module.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
+    <Background />
   </StrictMode>,
 )
