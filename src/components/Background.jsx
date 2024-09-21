b294e099d971fd51edd342515e67167fddeb5984
Sador <Zerezghi@live.co.uk>
2024-09-21 17:46:08 +0100

    Added darkmode for background header and body

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,19 +1,31 @@
+import { useEffect, useState } from "react";
+import moon from "../assets/images/icon-moon.svg";
+import sun from "../assets/images/icon-sun.svg";
 import styles from "../components/Background.module.css";
 import "../styles/Base.module.css";
-import moon from "../assets/images/icon-moon.svg";
-
 
 const Background = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode
+      ? "hsl(235, 21%, 11%)"
+      : "hsl(0, 0%, 98%)";
+  }, [darkMode]);
+
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
     <>
-      <main className={styles.container}>
-          <header>
-            <h1> TODO </h1>
-            <button>
-              <img src={moon}></img>
-            </button>
-          </header>
+      <main className={`${styles.container} ${darkMode ? styles.dark : ""}`}>
+        <header className={styles.title}>
+          <h1> TODO </h1>
+          <button type="button" onClick={handleDarkMode}>
+            <img src={darkMode ? sun : moon}></img>
+          </button>
+        </header>
       </main>
     </>
   );
