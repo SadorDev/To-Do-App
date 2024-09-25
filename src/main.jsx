05852ff04e70f869677310537fc41f275c63038d
Sador <Zerezghi@live.co.uk>
2024-09-25 07:55:08 +0100

    All components added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Background from "./components/Background.jsx"
-import "./styles/Base.module.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Background from "./components/Background.jsx";
+import ToDoItem from "./components/ToDoItem.jsx";
+import ToDoList from "./components/ToDoList.jsx";
+// import Filter from "./components/Filter.jsx";
+import "./styles/Base.module.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Background />
-  </StrictMode>,
-)
+     <ToDoItem />
+     <ToDoList />
+     {/* <Filter />  */}
+  </StrictMode>
+);
