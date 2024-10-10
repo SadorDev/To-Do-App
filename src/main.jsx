077ad0a51f50598a2b23085f5e00746169ce2e87
Sador <Zerezghi@live.co.uk>
2024-10-10 02:07:55 +0100

    Removed ToDoItem for functionality purposes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Background from "./components/Background.jsx";
-import ToDoItem from "./components/ToDoItem.jsx";
 import ToDoList from "./components/ToDoList.jsx";
 // import Filter from "./components/Filter.jsx";
 import "./styles/Base.module.css";
@@ -9,7 +8,6 @@ import "./styles/Base.module.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Background />
-     <ToDoItem />
      <ToDoList />
      {/* <Filter />  */}
   </StrictMode>
