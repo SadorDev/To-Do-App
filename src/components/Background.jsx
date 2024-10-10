f722402b4f82e3d8bb270460269f403d63e15f3b
Sador <Zerezghi@live.co.uk>
2024-10-10 02:07:11 +0100

    Changed classname

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -8,9 +8,7 @@ const Background = () => {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    document.body.style.backgroundColor = darkMode
-      ? "hsl(235, 21%, 11%)"
-      : "hsl(0, 0%, 98%)";
+    document.body.style.backgroundColor = darkMode ? "hsl(235, 21%, 11%)" : "hsl(0, 0%, 98%)";
   }, [darkMode]);
 
   const handleDarkMode = () => {
@@ -19,7 +17,7 @@ const Background = () => {
 
   return (
     <>
-      <main className={`${styles.container} ${darkMode ? styles.dark : ""}`}>
+      <main className={`${styles.background_container} ${darkMode ? styles.dark : ""}`}>
         <header className={styles.title}>
           <h1> TODO </h1>
           <button type="button" onClick={handleDarkMode}>
