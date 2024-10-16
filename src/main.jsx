69a56dbb6773fad2dd1023991d3e01b45177bac3
Sador <Zerezghi@live.co.uk>
2024-10-16 05:57:51 +0100

    Added Filter Component

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Background from "./components/Background.jsx";
 import ToDoList from "./components/ToDoList.jsx";
-// import Filter from "./components/Filter.jsx";
+import Filter from "./components/Filter.jsx";
 import "./styles/Base.module.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Background />
      <ToDoList />
-     {/* <Filter />  */}
+     <Filter /> 
   </StrictMode>
 );
