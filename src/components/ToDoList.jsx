3a4a5ae4dca237c2b58bdb3d5b2fadb604ef4465
Sador <Zerezghi@live.co.uk>
2024-10-16 06:01:13 +0100

    Template syntax but not yet completed

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import AddToDo from "./AddToDo";
+import styles from "../components/ToDoList.module.css";
+import completed from "../assets/images/icon-check.svg";
 
 const ToDoList = () => {
   const [toDos, setToDos] = useState([]);
@@ -9,14 +11,17 @@ const ToDoList = () => {
   };
 
   return (
-    <>
-      <AddToDo handleSubmit={handleSubmit} />
+    <div className={styles.todoContainer}>
+        <AddToDo handleSubmit={handleSubmit} />
       <ul>
         {toDos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+          <li className={styles.list} key={index}>
+            <img src={completed} alt="active check icon" />
+            {todo}
+          </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
 
